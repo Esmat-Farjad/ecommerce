d798d606f2c52f1a5728104b525b02d83ec923cd
Seddiqui <ab.mateenseddiqui@gmail.com>
2025-03-09 14:52:39 +0430

    purchase module restructured

--- store/forms.py
+++ store/forms.py
@@ -31,21 +31,12 @@ class StoreCreationForm(forms.ModelForm):
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'border py-2 border-teal-500 w-full'
             
+            
 class PurchaseProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
-        exclude = ['user','profit','stock','rate']
-        widgets = {
-            'mfd':forms.widgets.DateInput(
-                attrs={
-                    "type": "date",
-                }),
-            'expd':forms.widgets.DateInput(
-                attrs={
-                    "type": "date",
-                })
-        }
+        exclude = ['user','total_items','total_package_price', 'item_sale_price']
         
     def __init__(self, *args, **kwargs):
         super(PurchaseProductForm, self).__init__(*args, **kwargs)
