39aa472750ada348f8e07ecc7d5e2541c84f519d
Seddiqui <ab.mateenseddiqui@gmail.com>
2025-03-10 23:44:29 +0430

    Update purchase issue resolved

--- store/forms.py
+++ store/forms.py
@@ -44,6 +44,17 @@ class PurchaseProductForm(forms.ModelForm):
             visible.field.widget.attrs['class']="form-input"
             visible.field.widget.attrs['placeholder']=visible.field.label
 
+class UpdateProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
+        exclude = ['user']
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateProductForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = "form-input"
+            visible.field.widget.attrs['placeholder'] = visible.field.label
 class ProductSearchForm(forms.Form):
     query = forms.CharField(
         label="Search", 
@@ -56,20 +67,6 @@ class ProductSearchForm(forms.Form):
         )
 
 
-class UpdateProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = "__all__"
-        widgets = {
-            'mfd':forms.widgets.DateInput(
-                attrs={
-                    "type": "date",
-                }),
-            'expd':forms.widgets.DateInput(
-                attrs={
-                    "type": "date",
-                })
-        }
         
     def __init__(self, *args, **kwargs):
         super(UpdateProductForm, self).__init__(*args, **kwargs)
